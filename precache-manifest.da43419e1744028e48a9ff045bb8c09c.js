548dd2a27405f7ef5646a5c6519eff1f1303f184
jinju0809 <jjyoun48@gmail.com>
2021-03-08 18:23:36 +0900

    Updates

---
+++ precache-manifest.da43419e1744028e48a9ff045bb8c09c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86ba812f9316a703be81",
+    "url": "/pearl/static/css/main.8073b103.chunk.css"
+  },
+  {
+    "revision": "86ba812f9316a703be81",
+    "url": "/pearl/static/js/main.86ba812f.chunk.js"
+  },
+  {
+    "revision": "d76ea2e8c4f52a1c581b",
+    "url": "/pearl/static/js/runtime~main.d76ea2e8.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/pearl/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "396a652ca6d18bd65119859d8885afe8",
+    "url": "/pearl/index.html"
+  }
+];
