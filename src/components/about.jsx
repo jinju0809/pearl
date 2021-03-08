3f1ccddf5f4c9df40b25447050910a57c45452f3
jinju0809 <jjyoun48@gmail.com>
2021-03-08 18:15:58 +0900

    first commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,9 +11,9 @@ export default class About extends Component {
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                 <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
-                    <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
+                    <span className="heading-meta">About</span>
+                    <h2 className="colorlib-heading">생각하는 개발자 윤진주입니다.</h2>
+                    <p>새로운 지식을 습득하기 좋아하고</p>
                     <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
                     </div>
                 </div>
